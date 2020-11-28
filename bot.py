f32c5f42cc60b29b687117e51070180931580559
Joshua Smallwood <31932412+smallwoj@users.noreply.github.com>
2020-11-27 21:59:26 -0600

    Upload join sound via discord

--- bot.py
+++ bot.py
@@ -3,6 +3,10 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+from pytube import YouTube
+from db import Database
+
+db = Database()
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -12,7 +16,17 @@ bot = commands.Bot(command_prefix='j!')
 @bot.command(name='ping')
 async def pong(ctx):
     print(ctx.author.id)
-    await ctx.send('Pong!')
+    await ctx.send(f'Pong! ({bot.latency})')
+
+@bot.command(name='set')
+async def set_sound(ctx, link):
+    yt = YouTube(link)
+    sound = yt.streams.filter(type='audio').first()
+    if sound.filesize < 100000:
+        db.upload_sound(ctx.author.id, sound)
+        await ctx.send("Successful!")
+    else:
+        await ctx.send("too big")
 
 @bot.event
 async def on_ready():
--- db.py
+++ db.py
@@ -9,18 +9,25 @@ class Database:
             password='123456',
             database='joinbot'
         )
+        cursor = self.db.cursor()
+        cursor.execute('SHOW TABLES')
+        rows = cursor.fetchall()
+        rows = list(map(lambda x: x[0], rows))
+        cursor.close()
+        if 'join_sounds' not in rows:
+            self.create_table()
 
     def create_table(self):
         cursor = self.db.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS join_sounds ' + 
             '(' + 
             ', '.join([
-                'id, INT AUTO_INCREMENT PRIMARY KEY',
-                'discord_id, VARCHAR(255) UNIQUE',
-                'file_path, VARCHAR(255)',
+                'id INT AUTO_INCREMENT PRIMARY KEY',
+                'discord_id VARCHAR(255) UNIQUE',
+                'file_path VARCHAR(255)',
             ]) + ')'
         )
-        cursor.execute('CREATE INDEX discord_id ON join_sounds (discord_id)')
+        cursor.execute('CREATE INDEX discord_idx ON join_sounds (discord_id)')
         cursor.close()
 
     def has_sound(self, discord_id: str) -> bool:
@@ -48,12 +55,16 @@ class Database:
 
     def upload_sound(self, discord_id: str, sound: Stream):
         path = os.path.join('media', f'{discord_id}')
-        file_name = 'joinsound.mp3'
+        file_name = 'joinsound'
         sound.download(path, file_name)
-        if self.has_sound(discord_id):
-            query = f"UPDATE join_sounds SET path='{os.path.join(path, file_name)}' WHERE discord_id='{discord_id}'"
-        else:
-            query = f"INSERT INTO join_sounds (discord_id, path) VALUES ('{discord_id}', '{os.path.join(path, file_name)}')"
+        full_path = sound.get_file_path(output_path=path, filename=file_name)
         cursor = self.db.cursor()
-        cursor.execute(query)
+        if self.has_sound(discord_id):
+            query = f"UPDATE join_sounds SET file_path='{full_path}' WHERE discord_id='{discord_id}'"
+            cursor.execute(query)
+        else:
+            query = f"INSERT INTO join_sounds (discord_id, file_path) VALUES (%s, %s)"
+            val = (discord_id, full_path)
+            cursor.execute(query, val)
         cursor.close()
+        self.db.commit()
